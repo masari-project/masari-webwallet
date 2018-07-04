30cef4979edeff84494b04441fe335ce4e9600bb
gnock <gnock@protonmail.com>
2018-07-04 09:29:40 +0200

    Fix for import page translations not being properly routed

--- src/pages/import.ts
+++ src/pages/import.ts
@@ -15,5 +15,16 @@
 
 
 import {AppState} from "../model/AppState";
+import {DestructableView} from "../lib/numbersLab/DestructableView";
+
+class ImportView extends DestructableView{
+	constructor(container : string){
+		super(container);
+		let self = this;
+	}
+
+}
+
+new ImportView('#app');
 
 AppState.enableLeftMenu();
